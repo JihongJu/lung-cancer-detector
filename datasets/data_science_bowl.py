7138361499f63c3a2f0e80eaf2eb2f151189be17
Jihong Ju <daniel.jihong.ju@gmail.com>
2017-02-26 17:40:31 +0100

    [Add] load image todo

--- datasets/data_science_bowl.py
+++ datasets/data_science_bowl.py
@@ -5,16 +5,17 @@ import pandas as pd
 import nipy as ni
 import nibabel as nib
 from nipy.core.api import Image, vox2mni
+from sklearn.model_selection import train_test_split
 
 
 def load_data(trainval='stage1', img_format='dicom',
-              target_size=(512, 512, 128)):
+              target_size=(512, 512, 128), test_size=0.2):
     """Loads datasets of Data Science Bowl 2017
     # Arguments
         trainval: specify which set of images for train and validation
                   (stage1, sample_images)
     # Returns
-        Tuple of Numpy arrays: `(x, y)`.
+        Tuples of Numpy arrays: `(X_train, y_train), (X_test, y_test)`.
     Overview of the data directory structure:
     \root
         \data
@@ -31,14 +32,13 @@ def load_data(trainval='stage1', img_format='dicom',
     dirname = 'data-science-bowl'
     path = os.path.join('data', dirname)
 
-    # load images
+    # load csv
     df_labels = pd.read_csv(path+"/{}_labels.csv".format(trainval))
-
+    # load images
     nb_series = len(df_labels)
-    x = np.zeros((nb_series,)+target_size, dtype='float16')
-
+    X = np.zeros((nb_series,)+target_size, dtype='float16')
     for s_id, series in zip(df_labels.index, df_labels['id']):
-        # load image
+        # load image as nipy.core.image.image.Image
         if img_format == 'nii':
             img_path = os.path.join(path, img_format, trainval,
                                     'Axial_{}.nii.gz'.format(series))
@@ -52,12 +52,16 @@ def load_data(trainval='stage1', img_format='dicom',
                 dc = dicom.read_file(os.path.join(img_path, filename))
                 x_series[..., f_id] = dc.pixel_array
             img = Image(x_series, vox2mni(np.eye(4)))
-        # Coeregistration / normalization
+        # TODO: fix size and Coeregistration / normalization
         # See: http://nipy.org/nipy/devel/code_discussions/usecases/images.html
+        X[s_id, ...] = np.zeros(target_size)
+        print type(img), img.shape
 
-        print img.shape
-        print type(img)
+    # load labels
+    y = df_labels['cancer'].values.astype('int8')
 
-    x = []
-    y = []
-    return x, y
+    # split for train, test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=42)
+
+    return (X_train, y_train), (X_test, y_test)
--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -1,10 +1,10 @@
 import pytest
-from ..datasets.data_science_bowl import load_data
+from datasets.data_science_bowl import load_data
 
 
 def test_data_science_bowl():
     trainval = 'sample_images'
 
-    x, y = load_data(trainval=trainval, dimension=3)
-    print(len(x), len(y))
-    assert len(x) == len(y)
+    (X_train, y_train), (X_test, y_test) = load_data(trainval=trainval)
+    print(len(X_train), len(y_train))
+    assert len(X_train) == len(y_train)
