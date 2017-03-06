311d2c8a6d5dda866189d52cdf20320f4e77e0c9
Jihong Ju <daniel.jihong.ju@gmail.com>
2017-03-06 23:42:51 +0100

    Add output

--- tools/initialise_gpu_instance.sh
+++ tools/initialise_gpu_instance.sh
@@ -1,8 +1,14 @@
-# install docker
+# Ubuntu 16.04 GPU-instance with Nvidia driver properly installed
+# Maintainer: Jihong
+#
+# ==================================== #
+
+# Add docker's repo
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
+# Install
 sudo apt-get install -y docker-engine
 # add user to docker group
 sudo usermod -aG docker $USER
