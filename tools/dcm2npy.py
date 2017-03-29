d57d7b9f034322739c0c08d293c2c267be06c525
jihongju <daniel.jihong.ju@gmail.com>
2017-03-29 14:16:06 +0200

    segmenting lungs for preprocess

--- tools/dcm2npy.py
+++ tools/dcm2npy.py
@@ -1,10 +1,16 @@
-import os
+import numpy as np # linear algebra
+import pandas as pd
 import dicom
-import scipy
+import os
 import scipy.ndimage
-import numpy as np
 
 from multiprocessing import Pool, cpu_count
+from skimage import measure, morphology
+
+# Some constants
+INPUT_FOLDER = '../data/DataScienceBowl2017/dcm/stage1/'
+OUTPUT_FOLDER = '../data/DataScienceBowl2017/npy/stage1/'
+
 
 # Load the scans in given folder path
 def load_scan(path):
@@ -44,7 +50,7 @@ def get_pixels_hu(slices):
 
     return np.array(image, dtype=np.int16)
 
-def resample(image, scan, new_spacing=[2,2,2]):
+def resample(image, scan, new_spacing=[1,1,1]):
     # Determine current pixel spacing
     spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
 
@@ -58,32 +64,81 @@ def resample(image, scan, new_spacing=[2,2,2]):
 
     return image, new_spacing
 
-def preprocessing(path):
-    patient = load_scan(path)
-    patient_pixels = get_pixels_hu(patient)
-    pix_resampled, spacing = resample(patient_pixels, patient, [2,2,2])
-    print("HU converting: ({}, {}) "
-          "--> ({}, {})".format(np.amin(patient[0].pixel_array),
-                                np.amax(patient[0].pixel_array),
-                                np.amin(patient_pixels),
-                                np.amax(patient_pixels)))
-    print("Shape resampling: {} --> {}".format(patient_pixels.shape,
-                                               pix_resampled.shape))
-    return pix_resampled
+
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)
+
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]
+
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+
+    # not actually binary, but 1 and 2.
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image > -320, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+
+    # Pick the pixel in the very corner to determine which label is air.
+    #   Improvement: Pick multiple background labels from around the patient
+    #   More resistant to "trays" on which the patient lays cutting the air
+    #   around the person in half
+    background_label = labels[0,0,0]
+
+    #Fill the air around the person
+    binary_image[background_label == labels] = 2
 
 
-TRAINVAL = 'stage1'
-INPUT_FOLDER = '../data/data-science-bowl/dcm/{}'.format(TRAINVAL)
-OUTPUT_FOLDER = '../data/data-science-bowl/npy/{}'.format(TRAINVAL)
-patients = os.listdir(INPUT_FOLDER)
+    # Method of filling the lung structures (that is superior to something like
+    # morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
 
-def f(p):
-    img_path = os.path.join(INPUT_FOLDER, p)
-    pix_resampled = preprocessing(img_path)
-    output_path = os.path.join(OUTPUT_FOLDER, p)
-    np.save(output_path, pix_resampled)
-    return pix_resampled.shape
+            if l_max is not None: #This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
 
-nb_cpus = cpu_count()
-pool = Pool(processes=nb_cpus)
-results = pool.map(f, patients)
+
+    binary_image -= 1 #Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+
+    # Remove other air pockets insided body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+
+    return binary_image
+
+
+def preprocess(patient):
+    print("Processing {}".format(patient))
+    patient_scan = load_scan(INPUT_FOLDER + patient)
+    patient_pixels = get_pixels_hu(patient_scan)
+    pix_resampled, spacing = resample(patient_pixels, patient_scan, [4, 4, 4])
+    segmented_lungs_fill = segment_lung_mask(pix_resampled, True)
+    segmented_lungs_fill = morphology.binary_dilation(segmented_lungs_fill)
+    pix_segmented = pix_resampled.copy()
+    pix_segmented[segmented_lungs_fill == 0] = -1024
+    print("Saving {} with shape {}".format(patient, pix_segmented.shape))
+    np.save(os.path.join(OUTPUT_FOLDER, patient), pix_segmented)
+    return pix_segmented
+
+
+if __name__ == "__main__":
+    #patients = os.listdir(INPUT_FOLDER)
+    #patients.sort()
+    stage1_train = pd.read_csv('../data/DataScienceBowl2017/stage1_labels.csv')['id'].values
+    stage1_test = pd.read_csv('../data/DataScienceBowl2017/stage1_sample_submission.csv')['id'].values
+    patients = list(stage1_train) + list(stage1_test)
+    print("Overall {} patients".format(len(patients)))
+    pool = Pool(cpu_count())
+    results = pool.map(preprocess, patients)
+    print("Finished!")
