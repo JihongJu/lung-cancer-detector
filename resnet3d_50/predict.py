2cd3b0cad8a361246f6f569711d569524a01601b
jihongju <daniel.jihong.ju@gmail.com>
2017-03-30 01:29:05 +0200

    Train adoring jepsen

--- resnet3d_50/predict.py
+++ resnet3d_50/predict.py
@@ -3,8 +3,8 @@
 
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
+import re
 import argparse
-import numpy as np
 import pandas as pd
 from keras.models import load_model
 from preprocessing.volume_image import (
@@ -24,36 +24,30 @@ parser.add_argument('weights', help='Trained weights')
 args = parser.parse_args()
 if args.weights:
     weights = args.weights
+    title = re.sub('^output/resnet50_[a-z]+_', '', weights.strip('.h5'))
+    print("Prediction by {}.".format(title))
 
 test_datagen = VolumeImageDataGenerator(
-        **init_args['volume_image_data_generator']['test'])
+        **init_args['volume_image_data_generator']['test']['init'])
 
 test_vol_loader = NPYDataLoader(
         **init_args['volume_image_data_loader']['test'])
 
-iterator_args = init_args['volume_image_data_generator']['flow_from_loader']
-test_iter_args = iterator_args.copy()
+test_iter_args = init_args['volume_image_data_generator']['test']['flow_from_loader']
 test_iter_args['volume_image_data_loader'] = test_vol_loader
 
 
 model = load_model(weights)
 
+model_pred_args = init_args['model']['predict_generator']
+model_pred_args['generator'] = test_datagen.flow_from_loader(
+        **test_iter_args)
+predictions = model.predict_generator(**model_pred_args)
+print(predictions.flatten())
+print(len(predictions))
 
 df_subm = pd.DataFrame(columns=['id', 'cancer'])
-for idx, fn in enumerate(test_vol_loader.filenames):
-    x = test_vol_loader.load(fn)
-    x = test_datagen.standardize(x)
-    x = x[np.newaxis, ...]
-    print("Predicting {} (batch shape: {})".format(fn, x.shape))
-    proba = model.test(x, batch_size=1)
-    print(proba)
-    if proba.shape[-1] > 1:
-        y = proba.argmax(axis=-1)
-    else:
-        y = (proba > 0.5).astype('int32')
-    y = y[0][0]
-    print("Prediction: {fn}, {proba}, {y} ".format(fn=fn, proba=proba, y=y))
-    df_subm.loc[idx, 'id'] = fn
-    df_subm.loc[idx, 'cancer'] = proba[0][0]
+df_subm['id'] = test_vol_loader.patients
+df_subm['cancer'] = predictions.flatten()
 
-df_subm.to_csv('output/stage1_submission_resnet50.csv', index=False)
+df_subm.to_csv('output/stage1_submission_resnet50_{}.csv'.format(title), index=False)
