3df6fffb5f906aa91bb234a1cc206ad9fa467f2f
jihongju <daniel.jihong.ju@gmail.com>
2017-04-10 17:49:01 +0200

    Update resnet3d_18 training script

--- resnet3d_18/trainval.py
+++ resnet3d_18/trainval.py
@@ -17,7 +17,7 @@ from preprocessing.volume_image import (
     VolumeImageDataGenerator)
 from preprocessing.image_loader import (
     NPYDataLoader)
-from models.resnet3d import Resnet3DBuilder
+from resnet3d import Resnet3DBuilder
 
 import yaml
 with open("init_args.yml", 'r') as stream:
@@ -39,51 +39,51 @@ args = parser.parse_args()
 if args.t:
     title = args.t
 
-nb_classes = init_args['volume_image_data_generator'][
+nb_classes = init_args['volume_image_data_generator']['train'][
     'flow_from_loader']['nb_classes']
 
 
 checkpointer = ModelCheckpoint(
-    filepath="/tmp/resnet18_weights_{}.hdf5".format(title),
+    filepath="output/resnet18_checkpoint_{}.h5".format(title),
     verbose=1,
     save_best_only=True)
 lr_reducer = ReduceLROnPlateau(monitor='val_loss',
                                factor=np.sqrt(0.1),
                                cooldown=0,
-                               patience=5, min_lr=0.5e-6)
+                               patience=10, min_lr=1e-6)
 early_stopper = EarlyStopping(monitor='val_loss',
                               min_delta=0.001,
-                              patience=10)
+                              patience=50)
 csv_logger = CSVLogger(
     'output/{}_{}.csv'.format(datetime.datetime.now().isoformat(), title))
 
 train_datagen = VolumeImageDataGenerator(
-        **init_args['volume_image_data_generator']['train'])
-test_datagen = VolumeImageDataGenerator(
-        **init_args['volume_image_data_generator']['test'])
+        **init_args['volume_image_data_generator']['train']['init'])
+val_datagen = VolumeImageDataGenerator(
+        **init_args['volume_image_data_generator']['val']['init'])
 
 train_vol_loader = NPYDataLoader(
         **init_args['volume_image_data_loader']['train'])
 val_vol_loader = NPYDataLoader(
         **init_args['volume_image_data_loader']['val'])
 
-iterator_args = init_args['volume_image_data_generator']['flow_from_loader']
-train_iter_args = iterator_args.copy()
+train_iter_args = init_args['volume_image_data_generator']['train']['flow_from_loader']
 train_iter_args['volume_image_data_loader'] = train_vol_loader
-val_iter_args = iterator_args.copy()
+val_iter_args = init_args['volume_image_data_generator']['val']['flow_from_loader']
 val_iter_args['volume_image_data_loader'] = val_vol_loader
 
 image_shape = train_datagen.image_shape
-model = Resnet3DBuilder.build_resnet_18(image_shape, nb_classes)
+regularization_factor = 2.5e-2
+model = Resnet3DBuilder.build_resnet_18(image_shape, nb_classes, regularization_factor)
 compile_args = init_args['model']['compile']
 compile_args['optimizer'] = Adam(lr=1e-3)
 model.compile(**compile_args)
 
 model_fit_args = init_args['model']['fit_generator']
 model_fit_args['generator'] = train_datagen.flow_from_loader(**train_iter_args)
-model_fit_args['validation_data'] = test_datagen.flow_from_loader(
+model_fit_args['validation_data'] = val_datagen.flow_from_loader(
     **val_iter_args)
-model_fit_args['callbacks'] = [lr_reducer, early_stopper, csv_logger]
+model_fit_args['callbacks'] = [checkpointer, lr_reducer, early_stopper, csv_logger]
 
 model.fit_generator(**model_fit_args)
 model.save('output/resnet18_{}.h5'.format(title))
